2ea6bb74160942f6070e8efc145bcf1b1252cbfb
Jeremy Caldwell <jeremy@eternalistic.net>
2022-12-06 21:01:58 -0800

    Added address

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,8 @@ export default function Home() {
 
       <main>
         <Header title="Caldwell Farm" />
+        <h2>10914 East Richard Lane<br>
+        Spokane Valley, WA 99206</h2>
         <p className="description">
           To place an order please call 509-713-2333 or fill out the form below.
         </p>
