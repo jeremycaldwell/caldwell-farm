ce237eed58ab1cbca70f1cdd2e8e459de5f9538a
Jeremy Caldwell <jeremy@eternalistic.net>
2022-12-07 05:42:48 +0000

    Updated form name

--- components/EggOrderForm.js
+++ components/EggOrderForm.js
@@ -6,7 +6,7 @@ export default function EggOrderForm() {
         className={styles.form}
         data-netlify="true"
         data-netlify-honeypot="bot-field"
-        name="feedback"
+        name="eggOrderForm"
         method="POST"
         action="/success"
       >
