776606dc6e9b35b0cc074483c851b4d034c332c6
Jeremy Caldwell <jeremy@eternalistic.net>
2023-01-12 08:49:53 -0800

    Updated favicons

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,10 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Caldwell Farm Egg Order Form</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main>
