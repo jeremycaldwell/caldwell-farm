82c0c7deb9d37fc5a573eb9e70972c2a59b1cea2
Jeremy Caldwell <jeremy@eternalistic.net>
2024-12-14 12:28:57 -0800

    Fixed other className instances

--- pages/success.js
+++ pages/success.js
@@ -4,25 +4,25 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div classNameName="container">
       <Head>
         <title>Caldwell Farm Egg Order Form</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <header class="header">
+        <header className="header">
           <Header title="Success!" />
-          <div class="description">
+          <div className="description">
             <p>Thank you for your order.</p>
             <p>We will collect and clean the eggs then let you know when they are ready for pickup or delivery.</p>
             <p><b>Online payment methods</b></p>
             <p>
-              <a href="https://cash.app/$jeremymcaldwell" class="icon-with-link">
+              <a href="https://cash.app/$jeremymcaldwell" className="icon-with-link">
                 <img src="/icon-square-cash.svg" alt="Cash App icon" width="30px" />
                 Cash App
               </a>
-              <a href="https://account.venmo.com/u/eternalistic" class="icon-with-link">
+              <a href="https://account.venmo.com/u/eternalistic" className="icon-with-link">
                 <img src="/venmo-logo.svg" alt="Venmo App icon" width="30px" />
                 Venmo
               </a>
